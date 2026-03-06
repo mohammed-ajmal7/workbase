218be1d338f76dc200ced1cc84cd9d7d4829d4e1
Ajmal <mmohammedajmal7@gmail.com>
2026-03-06 11:31:41 +0530

    Redux toolkit initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import ReduxProvider from "@/redux/providers/ReduxProvider";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
     </html>
   );
 }
