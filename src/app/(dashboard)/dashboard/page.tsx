893457d568bb8c5a6cd25ab1ac44a2e8d7115d3d
Mohammed Ajmal <141860995+mohammed-ajmal7@users.noreply.github.com>
2026-03-26 12:03:02 +0530

    Revert "Feature/frontend dashboard components"

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,22 +1,10 @@
 import DashboardCard from "@/components/DashboardCard/DashboardCard";
-import ProjectAnalytics from "@/components/ProjectAnalytics/ProjectAnalytics";
-import RecentActivities from "@/components/recentActivities/recentActivities";
-import { DASHBOARD_CARD_DATA, recentActivitiesData } from "@/constants/dashboard.constant";
+import { DASHBOARD_CARD_DATA } from "@/constants/dashboard.constant";
 
 export default function page() {
   return (
-    <div className="flex flex-col gap-7">
+    <div>
       <DashboardCard Arrdata={DASHBOARD_CARD_DATA} />
-      <div className=" bg-white border-gray-200 border rounded-md p-5">
-        <h3 className="text-black text-s font-bold">Project Analytics</h3>
-        <p className="text-zinc-400 text-xs">
-          Project activity over the last 8 months
-        </p>
-        <ProjectAnalytics />
-      
-      </div>
-      
-        <RecentActivities data={recentActivitiesData} />
     </div>
   );
 }
